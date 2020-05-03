4dafaeaee8a12ad187492db2fc5380cf66a58801
acetil <acetildev@gmail.com>
2020-05-03 17:28:36 +1000

    Added behaviour template logic

--- src/main/java/acetil/modjam/common/tile/SuperDispenserBehaviour.java
+++ src/main/java/acetil/modjam/common/tile/SuperDispenserBehaviour.java
@@ -11,15 +11,53 @@ import java.util.function.Function;
 import java.util.function.Predicate;
 
 public class SuperDispenserBehaviour {
-    private static List<InitialBehaviour> initialBehaviours;
+    private static List<Behaviour> initialBehaviours;
+    private static List<Behaviour> effectBehaviours;
     public static void registerInitial (Predicate<ItemStack> itemPredicate, Function<ItemStack, ItemStack> stackFunction,
                                  TriFunction<World, BlockPos, Direction, Boolean> behaviour) {
-        initialBehaviours.add(0, new InitialBehaviour(itemPredicate, stackFunction, behaviour));
+        // for what happens when the item is dispensed
+        // itemstack function is what happens TO THE SLOT, and triggers only when behaviour returns true
+        // effects are added in order of increasing priority
+        // boolean is for the success of the behaviour.
+        initialBehaviours.add(0, new Behaviour(itemPredicate, stackFunction, behaviour));
     };
 
-    private static class InitialBehaviour {
-        public InitialBehaviour (Predicate<ItemStack> itemPredicate, Function<ItemStack, ItemStack> stackFunction,
-                                 TriFunction<World, BlockPos, Direction, Boolean> behaviour) {
+    public static void registerEffect (Predicate<ItemStack> itemPredicate, Function<ItemStack, ItemStack> stackFunction,
+                                             TriFunction<World, BlockPos, Direction, Boolean> behaviour) {
+        // for what happens after the dispensed entity contacts a block
+        // importantly, unlike initial itemstack function, this one is what happens to the dropped itemstack
+        effectBehaviours.add(0, new Behaviour(itemPredicate, stackFunction, behaviour));
+    }
+    public static ItemStack evaluateInitial (ItemStack stack, World world, BlockPos pos, Direction direction, boolean doContinue) {
+        ItemStack resultStack = stack;
+        for (Behaviour b : initialBehaviours) {
+            if (b.itemPredicate.test(stack)) {
+                boolean result = b.behaviour.apply(world, pos, direction);
+                if (result) {
+                    resultStack = b.stackFunction.apply(stack);
+                }
+                if (result || !doContinue) {
+                    break;
+                }
+            }
+        }
+        return resultStack;
+    }
+    public static ItemStack evaluateEffect (ItemStack stack, World world, BlockPos pos, Direction direction) {
+        ItemStack resultStack = stack;
+        for (Behaviour b : effectBehaviours) {
+            if (b.itemPredicate.test(stack)) {
+                if (b.behaviour.apply(world, pos, direction)) {
+                    resultStack = b.stackFunction.apply(stack);
+                    break;
+                }
+            }
+        }
+        return resultStack;
+    }
+    private static class Behaviour {
+        public Behaviour (Predicate<ItemStack> itemPredicate, Function<ItemStack, ItemStack> stackFunction,
+                          TriFunction<World, BlockPos, Direction, Boolean> behaviour) {
             this.itemPredicate = itemPredicate;
             this.stackFunction = stackFunction;
             this.behaviour = behaviour;
